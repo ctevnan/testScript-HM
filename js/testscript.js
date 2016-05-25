692c01f99906d3c7acb16a166453538543b86d92
ctevnan <carolyntevnan@yahoo.com>
2016-05-25 17:07:56 -0400

    added navbar, footer, listgroup, began ajax call

--- js/testscript.js
+++ js/testscript.js
@@ -1,1 +1,23 @@
 //create a runnable program and/or script to do testing
+//include creation of (POST) user profile/db of 4 images
+//then do a verification call 
+//the trigger command is match
+//api endpoint https://matchapi.halberdtechnologies.com
+
+$(Document).ready(function(){
+$.ajax
+type: POST,
+url: "https://matchapi.halberdtechnologies.com",
+success: function(userID)
+  for (var i = 0; i < userID.length; i++) {
+          var newListItem = buildListGroup([i]);
+          $(".list-group").append(newListItem);
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error");
+      }
+  });
+} 
+
+
