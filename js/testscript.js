b06d4208cd6a2f2b6c707da079403db86cf18095
ctevnan <carolyntevnan@yahoo.com>
2016-05-25 17:48:29 -0400

    page layout almost finished, working on functionality

--- js/testscript.js
+++ js/testscript.js
@@ -1,15 +1,35 @@
 //create a runnable program and/or script to do testing
 //include creation of (POST) user profile/db of 4 images
-//then do a verification call 
+//then do a verification call (/v)
 //the trigger command is match
 //api endpoint https://matchapi.halberdtechnologies.com
 
 $(Document).ready(function(){
 $.ajax
 type: POST,
-url: "https://matchapi.halberdtechnologies.com",
+url: "https://www.matchapi.halberdtechnologies.com",
 success: function(userID)
-  for (var i = 0; i < userID.length; i++) {
+  error: function(jqXHR, textStatus, errorThrown) {
+    alert("Error");
+    }
+  });
+} 
+
+$.ajax
+type: POST,
+url: "https://www.matchapi.halberdtechnologies.com",
+success: function(currentDB)
+  error: function(jqXHR, textStatus, errorThrown) {
+    alert("Error");
+    }
+  });
+} 
+
+$.ajax
+type: POST,
+url: "https://www.matchapi.halberdtechnologies.com",
+success: function(mode)
+  for (var i = 0; i < mode.length; i++) {
           var newListItem = buildListGroup([i]);
           $(".list-group").append(newListItem);
         }
@@ -19,5 +39,4 @@ success: function(userID)
       }
   });
 } 
-
 
