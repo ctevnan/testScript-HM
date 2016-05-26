2bd0b42d98ec289a3ced0bb40415f3845698abbc
ctevnan <carolyntevnan@yahoo.com>
2016-05-26 12:46:00 -0400

    redid ajax calls, changed api endpoint, not getting a payload of json data back yet

---
+++ buildlistgroup.js
@@ -0,0 +1,26 @@
+/*$(document).ready(function() {
+  $(".list-group").on("click", "a", function(e) {
+    e.preventDefault();
+    
+    $.ajax({
+      type: "GET",
+      url: $(this).attr("href"),
+      success: function(userID) {
+        $("tbody").empty();
+        for (var i = 0; i < userID.length; i++) {
+          $("tbody").append(buildTableRow(userID[i]));
+        }
+      }
+    })  
+  });
+
+  function buildTableRow(photoData) {
+    //var photoTd = $("<td>").append(commitData. (enter params) );
+    var userIDTd = $("<td>").append(commitData. (enter params here)   );
+    //var dateTd = $("<td>").append(commitData.commit.author.date);
+
+    return $("<tr>").append(   Td)
+      .append(     Td)
+      .append(    Td);
+  }  
+});*/
--- testing.js
+++ testing.js
@@ -3,43 +3,56 @@
 //then do a verification call (/v)
 //the trigger command is match
 //api endpoint https://matchapi.halberdtechnologies.com
+//ajax url specifies endpoint to hit, can be any CORS(cross origin resource sharing) enabled endpoint
+//get retrieves data from endpoint
+//post creates data
+//put updates data
+//delete deletes data
 
-$(Document).ready(function(){
-$.ajax
-type: "GET",
-//stringBaseurl = "https://matchapi.halberdtechnologies.com/api";
-//var userId = "f6cd6592-f61e-40b2-afcc-c38827f075df";
-//var command = stringBaseurl + "/database?userID=" + userID;
-url: "https://matchapi.halberdtechnologies.com/api/database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df",
-success: function(f6cd6592-f61e-40b2-afcc-c38827f075df)
-  error: function(jqXHR, textStatus, errorThrown) {
-    alert('Error');
-    }
-  });
+$(document).ready(function(){
+  $.ajax ({
+    type: "GET", //retrieves data 
+    url: "https://matchapi.halberdtechnologies.com/api/Database?userID={f6cd6592-f61e-40b2-afcc-c38827f075df}", //api endpoint to hit
+    success: function(data, textStatus, jqXHR){  //this function is called when the GET request is successful
+    console.log(data);  //data contains the response from the server
+    console.log(textStatus); //returns the status of the request in words
+    console.log(jqXHR);  //returns the raw XMLhttpRequest object
+  },  
+    error: function(jqXHR, textStatus, errorThrown){  //this function is called when the GET request fails
+    console.log(jqXHR);   //returns raw XMLhttpRequest object
+    console.log(textStatus);  //returns the status of the request in words
+    console.log(errorThrown);  //returns an error object
+  }  
+});
+
+
+ 
+
+$.ajax ({
+  type:"POST",
+  url: "https://www.matchapi.halberdtechnologies.com/api/Database?userID={f6cd6592-f61e-40b2-afcc-c38827f075df}&database={testdb}",
+  success: function(data, textStatus, jqXHR){
+  console.log(data);
+  console.log(textStatus);
+  console.log(jqXHR);
+},  
+  error: function(jqXHR, textStatus, errorThrown){
+  console.log(jqXHR);
+  console.log(textStatus);
+  console.log(errorThrown);
 } 
 
-$.ajax
-type:"POST",
-url: "https://www.matchapi.halberdtechnologies.com",
-success: function(currentDB)
-  error: function(jqXHR, textStatus, errorThrown) {
-    alert("Error");
-    }
-  });
-} 
-
-$.ajax
-type: "POST",
-url: "https://www.matchapi.halberdtechnologies.com",
-success: function(mode)
-  for (var i = 0; i < mode.length; i++) {
-          var newListItem = buildListGroup([i]);
-          $(".list-group").append(newListItem);
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        alert("Error");
-      }
-  });
-} 
-
+//$.ajax
+//type: "POST",
+//url: "https://www.matchapi.halberdtechnologies.com",
+//success: function(mode)
+//  for (var i = 0; i < mode.length; i++) {
+//          var newListItem = buildListGroup([i]);
+//          $(".list-group").append(newListItem);
+//        }
+//      },
+//      error: function(jqXHR, textStatus, errorThrown) {
+//        alert("Error");
+//      }
+//  });
+//} 
