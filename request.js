c7aa1fe7209a0cc978b9f0302844440048764ec2
ctevnan <carolyntevnan@yahoo.com>
2016-06-01 11:37:01 -0400

    deleted file, made changes api call format in request.js

--- request.js
+++ request.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-  $.ajax({
-    type: "GET",
-    url: "https://matchapi.halberdtechnologies.com/Help/Api/GET-api-Database_userID={f6cd6592-f61e-40b2-afcc-c38827f075df}",
-    success: function(userID){
-      console.log('f6cd6592-f61e-40b2-afcc-c38827f075df');
-    }
-  })
+  $(".btn-default").on("click", function(e){
+    e.preventDefault();
+    var userID = $("#userID").val();
+    var matchApiUrl = "https://matchapi.halberdtechnologies.com/Help/Api/GET-api-Database_userID={f6cd6592-f61e-40b2-afcc-c38827f075df}";
+    matchApiUrl += "&userID=" + userID;
+
+    $.ajax({
+      type: "GET",
+      url: "https://matchapi.halberdtechnologies.com/Help/Api/GET-api-Database_userID={f6cd6592-f61e-40b2-afcc-c38827f075df}",
+      success: function(response){
+        console.log(response);
+      }
+    })
+  });  
 });
