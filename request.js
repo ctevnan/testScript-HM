96d0c3fb91747e6f20ebfe2a791880b9667fa8df
ctevnan <carolyntevnan@yahoo.com>
2016-06-03 12:31:56 -0400

    edited var matchApiUrl to match api endpoint that was tested on hurl.it with status 200

--- request.js
+++ request.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   $(".btn-default").on("click", function(e){
     e.preventDefault();
     var userID = $("#userID").val();
-    var matchApiUrl = "https://matchapi.halberdtechnologies.com/Help/Api/GET-api-Database_userID={f6cd6592-f61e-40b2-afcc-c38827f075df}";
-    matchApiUrl += "&userID=" + userID;
+    var matchApiUrl = "http://matchapi.halberdtechnologies.com/api/Database?userID={f6cd6592-f61e-40b2-afcc-c38827f075df}";
+    matchApiUrl += "userID=" + userID;
 
     $.ajax({
       type: "GET",
-      url: "https://matchapi.halberdtechnologies.com/Help/Api/GET-api-Database_userID={f6cd6592-f61e-40b2-afcc-c38827f075df}",
+      url: "http://matchapi.halberdtechnologies.com/api/Database?userID={f6cd6592-f61e-40b2-afcc-c38827f075df}",
       success: function(response){
         console.log(response);
       }
